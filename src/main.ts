cf8cec5fab65de210f5f118aa97322279acf3af5
Joost Muller <joostmuller@gmail.com>
2021-03-20 15:22:02 +0100

    chore: initial commit
    Based on screepers/screeps-typescript-starter.

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ErrorMapper } from "utils/ErrorMapper"; // wraps loop to correct line numbers
+
+export const loop = ErrorMapper.wrapLoop(() => {
+  console.log(`Current game tick is ${Game.time}`);
+
+  // Automatically delete memory of missing creeps
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+
+});
